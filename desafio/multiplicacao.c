b9cb1bf099c0848400ef2f333392eea2f8de2dac
rubens <rubens.miranda@discente.ufg.br>
2022-10-27 15:50:50 -0300

    desafio

---
+++ desafio/multiplicacao.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+
+//funcao para soma dos numeros
+void soma(char *num1, char *num2, char *result);
+
+//controla a alocacao de memoria 
+void manage_mem();
+
+//aloca memoria
+void get_mem(char *p);
+
+//libera memoria 
+void free_mem(char *p);
+
+//recebe os numeros
+int get_numbers(char *numVect);
+
+
+
+
+int main()
+{
+    char num1[40], num2[40];
+    return 0;
+}
+
+void soma(char *num1, char *num2, char *result);
+
+void manage_mem();
+
+void get_mem(char *p);
+
+void free_mem(char *p);
+
+int get_numbers(char *numVect);
+{
+    int i, casas; 
+    scanf("%[^'\ ']", numVect);
+
+    return strlen(numVect);
+}
