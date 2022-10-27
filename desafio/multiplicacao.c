76ad7adc9d756c31c078b35fead9b8783485fa4b
Lordbrandon12 <caioaugustov123@gmail.com>
2022-10-27 19:59:11 -0300

    eliminadas funçoes de alocaçao de memoria e movido a logica da multiplicaçao para dentro de uma função chamada de multiplicação

--- desafio/multiplicacao.c
+++ desafio/multiplicacao.c
@@ -2,49 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 
+//recebe dois numeros em forma de string e os multiplica
+//@param *num1 numero 1
+//@param *num2 numero 2
+//@param *resultado resultado
+void multiplicacao(char *num1, char *num2, char *res);
 
-//funcao para soma dos numeros
-void soma(char *num1, char *num2, char *result);
-
-//controla a alocacao de memoria 
-void manage_mem();
-
-//aloca memoria
-void get_mem(char *p);
-
-//libera memoria 
-void free_mem(char *p);
-
-//recebe os numeros
+//recebe os numeros do terminal e retorna o numero de algarismos do numero
+//@param *numVect vetor de um numero
+//@return tamanho do algarismo
 int get_numbers(char *numVect);
-
-
-
 
 int main()
 {
     char num1[40], num2[40], res[100];
+
     int tam1, tam2; // numero de casas decimais de cada numero
     int casos;
     int i, j; // contadores
     int temp1, temp2; // numeros temporarios
+
     scanf("%d%*c", &casos);
+
     while(casos--) {
-        tam1=get_numbers(num1);
-        tam2=get_numbers(num2);
+        multiplicacao(num1, num2, res);
     }
 
     return 0;
 }
 
-void soma(char *num1, char *num2, char *result);
+void multiplicacao(char *num1, char *num2, char *resultado)
+{
+        int tam1, tam2;
 
-void manage_mem();
+        tam1 = get_numbers(num1);
+        tam2 = get_numbers(num2);
 
-void get_mem(char *p);
 
-void free_mem(char *p);
 
+}
 int get_numbers(char *numVect)
 { 
     scanf("%s", numVect);
